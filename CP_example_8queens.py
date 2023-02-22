a5222b8912e5d8d6dbdd7181909b5075a6053b9c
cyclist10997 <123704073+cyclist10997@users.noreply.github.com>
2023-02-22 14:38:00 -0500

    Create CP_example_8queens.py
    CP example from the coursera lecture.

---
+++ CP_example_8queens.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 12:38:01 2023
+
+@author: brend
+"""
+
+from ortools.sat.python import cp_model
+
+def queens_problem(board_size):
+    model = cp_model.CpModel()
+
+    # Variables
+    queens = [model.NewIntVar(lb=0, ub=board_size - 1, name="x%d" % i) for i in range(board_size)]
+
+    # Constraints
+    for i in range(board_size):
+        for j in range(i + 1, board_size):
+            model.Add(queens[i] != queens[j])  # same row
+            model.Add(queens[i] + i != queens[j] + j)   # upper diagonal
+            model.Add(queens[i] - i != queens[j] - j)  # lower diagonal
+
+    # Solve
+    solver = cp_model.CpSolver()
+    solver.Solve(model)
+
+    # Print solution
+    result = [solver.Value(queens[i]) for i in range(board_size)]
+    return result
+
+print(queens_problem(8))
